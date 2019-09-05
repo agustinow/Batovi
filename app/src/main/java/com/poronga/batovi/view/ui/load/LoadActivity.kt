16ee49836df83210ecd0a29c09a99d70f2d35afe
agustinodella <agustinodella@gmail.com>
2019-09-05 14:31:16 -0300

    Added colors and Internet permission

--- app/src/main/java/com/poronga/batovi/view/ui/load/LoadActivity.kt
+++ app/src/main/java/com/poronga/batovi/view/ui/load/LoadActivity.kt
@@ -1,9 +1,10 @@
-package com.poronga.batovi
+package com.poronga.batovi.view.ui.load
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.poronga.batovi.R
 
-class MainActivity : AppCompatActivity() {
+class LoadActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
